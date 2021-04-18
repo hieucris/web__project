9084bb0dc0d802db2affe69f20e57afac9463db1
Hieu Cris <hieuhk195@gmail.com>
2021-04-18 16:12:46 +0700

    last update--filnal front end

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
-  $('.fade').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    nextArrow:$('.btnSlider-next'),
-    prevArrow:$('.btnSlider-prev'),
-  });
 
+  // scroll on top
   $(window).scroll(function(){
     if ($(this).scrollTop() > 240) {
         $('.on__top').fadeIn();
@@ -22,8 +14,9 @@ $(document).ready(function () {
     return false;
   });
 
+  // scroll on top fix menu
   $(window).bind('scroll', function() {
-    var navHeight = $( window ).height()-450;
+    var navHeight = $( window ).height()-500;
       if ($(window).scrollTop() > navHeight) {
         $('.header__nav').addClass('fixed');
       }
@@ -32,72 +25,7 @@ $(document).ready(function () {
       }
   }); 
 
-  $('.responsive').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-   ]
-  });
-
-  $('.carousel__content').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-   ]
-  });
-
+  // slide story__wrap
   $('.story__wrap').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
@@ -106,6 +34,8 @@ $(document).ready(function () {
     nextArrow:$('.next-item'),
     prevArrow:$('.prev-item'),
   });
+
+  // slide sugges__wrap
   $('.sugges__wrap').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
@@ -114,6 +44,8 @@ $(document).ready(function () {
     nextArrow:$('.next-item__sugges'),
     prevArrow:$('.prev-item__sugges'),
   });
+
+  // slide category__wrap
   $('.category__wrap').slick({
     slidesToShow: 6,
     slidesToScroll: 3,
@@ -122,6 +54,8 @@ $(document).ready(function () {
     nextArrow:$('.next-item__category'),
     prevArrow:$('.prev-item__category'),
   });
+
+  // slide top__wrap
   $('.top__wrap').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
@@ -130,6 +64,8 @@ $(document).ready(function () {
     nextArrow:$('.next-item__top'),
     prevArrow:$('.prev-item__top'),
   });
+
+  // slide new__wrap
   $('.new__wrap').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
@@ -139,13 +75,10 @@ $(document).ready(function () {
     prevArrow:$('.prev-item__new'),
   });
   
+  // start wow
   new WOW().init();
-  $('.single-item').slick({
-    nextArrow:$('.btn__carousel--next'),
-    prevArrow:$('.btn__carousel--prev'),
-    cssEase:'cubic-bezier(.34,.45,.84,.55)',
-  });
 
+  // sort storyBook__nation
   $('.story-nation__wrap').isotope({
     itemSelector: '.slider__story',
   });
@@ -159,6 +92,8 @@ $(document).ready(function () {
     $('.story-nation__wrap').isotope({filter : nation});
     return false;
   });
+
+  // start load web
   
   $(window).on('load', function(){
     setTimeout(removeLoader, 700);
@@ -169,10 +104,29 @@ $(document).ready(function () {
     });  
   }
 
-  $(".search--control").focusin(function() {
-    $(".search__filter").addClass("search__show");
-  }).focusout(function () {
-    $(".search__filter").removeClass("search__show");
+  // Search filter 
+  $(document).ready(function () {
+    $(".search--control").keyup(function () {
+      let searchText = $(this).val();
+      if (searchText != "") {
+        $.ajax({
+          url: "action.php",
+          method: "post",
+          data: {
+            query: searchText,
+          },
+          success: function (response) {
+            $(".list__storyBook--filter").html(response);
+          },
+        });
+      } else {
+        $(".list__storyBook--filter").html("");
+      }
+    });
+    $(document).on("click", "a", function () {
+      $(".search--control").val($(this).text());
+      $(".list__storyBook--filter").html("");
+    });
   });
 
 });
