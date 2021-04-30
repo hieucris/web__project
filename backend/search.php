a8892c19808c1032c9a08e039be0a7412cc67d37
tinnt <tin.nguyen1@onemount.com>
2021-04-30 15:13:13 +0700

    test search engine

--- backend/search.php
+++ backend/search.php
@@ -12,10 +12,19 @@
           $result = $exec->get_result();  
   		if ($result->num_rows > 0) {
   			while ($row = $result->fetch_array()) {
-  				$output .= '<li>'.ucwords($row['title']).'</li>';
+  				$output .= '<li>';
+  				$output .= '	<a href="">';
+				$output .= "		<img src='' class='storyBook--filter-img'>";
+  				$output .= "			<div class='storyBook--filter-right'>";
+				$output .= "			<div class='storyBook--filter-name'>".$row["title"]."</div>";
+				$output .= "			<p class='storyBook--filter-content'>".$row["genre"]."</p>";
+				$output .= '			<p class="storyBook--filter-chapter"></p>'; //cần thêm db truyện
+				$output .= "		</div>";
+				$output .= "	</a>";
+				$output .= "</li>";
   			}
   		}else{
-  			  $output .= '<li> Comic not Found</li>';
+  			  $output .= '<a> Comic not Found</a>';
   		}
   		
 	  	$output .= '</ul>';
