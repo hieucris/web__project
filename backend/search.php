2daab41d85ae1374d0b8209b70ab9f2ac47f81d0
tinnt <tin.nguyen1@onemount.com>
2021-05-02 11:10:25 +0700

    test search engine

--- backend/search.php
+++ backend/search.php
@@ -1,30 +1,25 @@
 <?php
-	ini_set('display_errors', 1); 
-	error_reporting(E_ALL);
-    $conn = new mysqli("localhost", "tinnt", "123", "DB");
+    $conn = new mysqli("localhost", "tinnt", "123", "doctruyen");
     if($conn->connect_error){
         die("Connection Failed: ".$conn->connect_error);    
     }
     if(isset($_POST["comic"])){
   		$output = "";
-  		$title= $_POST['comic'];                            
-		// $exec = $conn->prepare("SELECT * FROM comics WHERE title LIKE '%?%'");
-		// $exec->bind_param("s",$title);
-		// $exec->execute();
-		// $result = $exec->get_result();  
-		$query = "SELECT * FROM comics WHERE title LIKE '%$title%'";
+  		$name = $_POST['comic'];                            
+		// $query = "SELECT * FROM stories WHERE name LIKE '%$name%'";
+		$query = "SELECT stories.name, categories.name, stories.id, stories.img FROM categories, stories WHERE stories.name LIKE '%$name%' AND stories.category_id = categories.id";
 		$result = $conn->query($query);                                    
 
 		$output .= '<ul class="list__storyBook--filter">';
   		if ($result->num_rows > 0) {
   			while ($row = $result->fetch_array()) {
   				$output .= '<li>';
-  				$output .= '	<a href="">';
-				$output .= "		<img src='' class='storyBook--filter-img'>";
+  				$output .= '	<a href="storyBook.php?id='.$row["2"].'">';
+				$output .= "		<img src='".$row["img"]."' class='storyBook--filter-img'>";
   				$output .= "			<div class='storyBook--filter-right'>";
-				$output .= "			<div class='storyBook--filter-name'>".$row["title"]."</div>";
-				$output .= "			<p class='storyBook--filter-content'>".$row["genre"]."</p>";
-				$output .= '			<p class="storyBook--filter-chapter"></p>'; //cần thêm db truyện
+				$output .= "			<div class='storyBook--filter-name'>".$row["0"]."</div>";
+				$output .= "			<p class='storyBook--filter-content'>".$row["1"]."</p>";
+				$output .= '			<p class="storyBook--filter-chapter"></p>'; 
 				$output .= "		</div>";
 				$output .= "	</a>";
 				$output .= "</li>";
